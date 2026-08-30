a492f1d2c48200c6306307c98ef111f8cddf1f51
ShraddhaAmbekar <ambekarshraddha21@gmail.com>
2026-08-30 21:51:38 +0530

    api key changed

--- frontend/src/pages/DataEntry/GateEntry.jsx
+++ frontend/src/pages/DataEntry/GateEntry.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 
-const API_URL = "http://localhost:5000";
+import  API_URL from "../api";
 
 function GateEntry() {
   // =====================================================
